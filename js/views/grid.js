a3eaf9289bbddba736540618e880878a1f6f9954
William Glass <william.glass@gmail.com>
2013-12-16 14:42:54 -0800

    Add more comments.

--- js/views/grid.js
+++ js/views/grid.js
@@ -1,6 +1,9 @@
 define(
     ["underscore", "zepto", "backbone"],
     function( _, $, Backbone ) {
+        // The GridView owes its simplicity to underscore's templating
+        // feature.  The dynamic markup template is located in index.html
+        // itself, we merely pass the grid model as context when rendering.
         var GridView = Backbone.View.extend({
             el: "#grid",
             template: _.template($("#grid-template").html()),
@@ -12,6 +15,9 @@ define(
                 return this;
             },
             handle_human_move: function( event ) {
+                // Whenever a cell in the grid is clicked on, we merely
+                // tell the grid model to place the human player's symbol
+                // at the clicked spot.
                 var cell = this.$(event.currentTarget);
 
                 this.model.grid.place_symbol(
